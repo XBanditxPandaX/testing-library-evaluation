1b2a5858117b4e2bfed50eaa88142fa62cbe4e80
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2025-04-15 09:31:20 +0200

    Création du test avec vérification présence Welcome home et Fill out the form + ajout du clic utilisateur

--- src/__tests__/app.jsx
+++ src/__tests__/app.jsx
@@ -1,18 +1,32 @@
-import { rest } from "msw"
-import { server } from "../../tests/server"
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {rest} from 'msw'
+import {server} from '../../tests/server'
+import App from '../app'
 
 beforeEach(() => {
   server.use(
-    rest.post("/form", (req, res, ctx) => {
+    rest.post('/form', (req, res, ctx) => {
       if (!req.body.food || !req.body.drink) {
         return res(
           ctx.status(400),
           ctx.json({
-            message: "les champs food et drink sont obligatoires",
+            message: 'les champs food et drink sont obligatoires',
           }),
         )
       }
-      return res(ctx.status(200), ctx.json({ data: req.body }))
+      return res(ctx.status(200), ctx.json({data: req.body}))
     }),
   )
+})
+
+test('Scénario 1 : Parcours complet du formulaire avec succès', async () => {
+  render(<App />)
+
+  expect(screen.getByText('Welcome home')).toBeInTheDocument()
+
+  const fillFormLink = screen.getByText('Fill out the form')
+  expect(fillFormLink).toBeInTheDocument()
+  userEvent.click(fillFormLink)
 })
